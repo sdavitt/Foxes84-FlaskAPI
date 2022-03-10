29ff200d93c385d0510eb94e121f49bf9e27018f
sdavitt <sdavt2@gmail.com>
2022-03-10 12:12:56 -0600

    jinja basics

--- app/routes.py
+++ app/routes.py
@@ -14,17 +14,21 @@ from app import app # import the app variable defined in __init__.py
     # if your route's job is to display an html page -> it's return value should be a call to render_template
 from flask import render_template
 
+# import choice for our random behaviour
+from random import choice
+
 @app.route('/') # decorator says: this function is a route of the flask app 'app' with the url endpoint '/'
 def home():
     print('Hello, Foxes!')
-    x = 5
-    print(f'X has value {x}. This will print in the terminal when this function runs when the url endpoint / is accessed.')
-    return render_template('index.html')
+    x = choice(['Patrick','Elif','Amir','Cameron','Kyle','Brandt','Brandon','Devon'])
+    print(f'X has value {x}. This value will be passed into render_template as a keyword argument and the keyword used in a jinja expression in index.html')
+    return render_template('index.html', name=x)
 
 
 # a second route!
 @app.route('/mcfc')
 def mancity():
-    x = 'Manchester City'
-    print(f'X has value {x}. This will print in the terminal when this function runs when the url endpoint /mcfc is accessed.')
-    return render_template('soccer.html')
+    headline = 'Manchester City beat Sporting Lisbon in the Champions League Round of 16!'
+    mcgoals = 'Five'
+    sportinggoals = 0
+    return render_template('soccer.html', headline=headline, mcfcg=mcgoals, slg=sportinggoals)
